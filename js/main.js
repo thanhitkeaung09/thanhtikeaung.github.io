dda2a2648788fce471484ea7285d1d78496356ad
Than Htike Aung <thanhtikeaung.gitacc@gmail.com>
2026-02-27 17:06:54 +0630

    feat : added chatbot ai

--- js/main.js
+++ js/main.js
@@ -304,6 +304,35 @@
 		owlCrouselFeatureSlide();
 	});
 
+	// Swiper JS
+// 	const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+   var swiper = new Swiper(".swiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+
 
 	// $('#contact-form').submit(function (e) {
     //     e.preventDefault();
@@ -339,6 +368,7 @@
     //   // alert('Oops…' + JSON.stringify(error));
     //   // })
     //   })
+	
 
 
 }());
